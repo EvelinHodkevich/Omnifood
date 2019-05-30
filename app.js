7a3bba96e969360c65caa1587be4453fb67bce54
Evelin Hodkevich <evelin.hodkevich@abv.bg>
2019-05-30 17:37:02 +0300

    NodeJS setup

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express"),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render('landing');
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    });
